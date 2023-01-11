e017bd7901047dd4b9209494d3c5a4867d5c95a1
Renan Farias <41553440+renansf36@users.noreply.github.com>
2023-01-10 23:15:12 -0300

    Inicio de Curso
    Composer, Autoloader

---
+++ src/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2e998ec04edffb31cdd06d82d171bf82c0a2caa0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2e998ec04edffb31cdd06d82d171bf82c0a2caa0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
