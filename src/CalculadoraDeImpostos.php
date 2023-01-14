7a48931674f8e7855841d6b0de69a48de3163a70
Renan Farias <41553440+renansf36@users.noreply.github.com>
2023-01-13 23:53:37 -0300

    Update CalculadoraDeImpostos.php

--- src/CalculadoraDeImpostos.php
+++ src/CalculadoraDeImpostos.php
@@ -1,16 +1,12 @@
 <?php
 
 namespace Alura\DesignPattern;
+use Alura\DesignPattern\Impostos\Imposto;
 
 class CalculadoraDeImpostos
 {
-    public function calcula(Orcamento $orcamento, string $nomeImposto): float
+    public function calcula(Orcamento $orcamento, Imposto $imposto): float
     {
-        switch($nomeImposto){
-            case "ICMS":
-                return $orcamento->valor * 0.1;
-            case "ICMS":
-                return $orcamento->valor * 0.06;
-        }
+        return $imposto->calculaImposto($orcamento);
     }
 }
